79645c95170e759351304af2b48ea94d1865ecfe
Noble-Phoenix <45508528+Noble-Phoenix@users.noreply.github.com>
2020-07-28 16:57:07 +0200

    Added player script that's WIP.

---
+++ GDC2020/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Fetches Rigidbody2D
+    Rigidbody2D m_Rigidbody;
+
+    // Sets the speed of the player
+    public int moveSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_Rigidbody.velocity = transform.forward * moveSpeed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
