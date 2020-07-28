1c2bc24799d0074544ecdacd8dd714c0ac391faf
WildclawsDK <alex-thomsen@hotmail.com>
2020-07-28 20:04:13 +0200

    Player movement, left/right, strafe and up/down

--- GDC2020/Assets/Scripts/Player.cs
+++ GDC2020/Assets/Scripts/Player.cs
@@ -23,19 +23,37 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        //Move();
-        if (Input.GetKey(KeyCode.W)){
-            transform.position += Vector3.right * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.position -= Vector3.right * Time.deltaTime;
-        }
+        Move();
+        Strafe();
+
+       // if (Input.GetKey(KeyCode.A)){
+         //   transform.position -= Vector3.right * Time.deltaTime;
+        // }
+       // else if (Input.GetKey(KeyCode.D))
+        //{
+      //      transform.position += Vector3.right * Time.deltaTime;
+       // }
     }
 
-    public void Move() {
+    public void Move()
+    {
         // Blue vector forward (blue) is the one which moves the player downward, so right vector(red) is used as forward instead.
         //Vector3 forwardMovement  = transform.right * Input.GetAxis("Vertical");
+
+      
+
+        Vector2 upwardMovement = transform.up * Input.GetAxis("Vertical") * moveSpeed;
+
+        myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0) + upwardMovement;
     }
 
+
+    public void Strafe()
+    {
+
+        Vector2 sideMovement = transform.right * Input.GetAxis("Horizontal") * moveSpeed;
+
+        myRigidbody.velocity = new Vector2(0, myRigidbody.velocity.y) + sideMovement;
+
+    }
 }
