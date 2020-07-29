e9c0ec1aa73454fa4908d57662c692ced7b0d332
WildclawsDK <alex-thomsen@hotmail.com>
2020-07-29 20:58:00 +0200

    Added Start Menu Implemented code for enemyAI

--- GDC2020/Assets/Scripts/Enemy.cs
+++ GDC2020/Assets/Scripts/Enemy.cs
@@ -8,19 +8,20 @@ public class Enemy : MonoBehaviour
 {
     public float enemySpeed = 4;
 
-
+    public Transform player;
     public Rigidbody enemyRigidbody;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRigidbody = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
     }
 
     public void OnCollisionEnter(Collision collision)
