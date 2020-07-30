28b6efc38544b38cf99563d8f590a6258bcc6826
WildclawsDK <alex-thomsen@hotmail.com>
2020-07-30 14:13:15 +0200

    Added Return to MainMenuButton to GameOverScene

---
+++ GDC2020/Assets/Scripts/Returner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Returner : MonoBehaviour
+{
+    public void ReturnButtonClicked()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
