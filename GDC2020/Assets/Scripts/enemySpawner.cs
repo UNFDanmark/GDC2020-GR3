f9155e322cd6dd0718d530cf5a935d0516dd8cef
WildclawsDK <alex-thomsen@hotmail.com>
2020-07-30 22:15:16 +0200

    Edited enemy spawner

---
+++ GDC2020/Assets/Scripts/enemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public int nrOfEnemies = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for (int i = 1; i < nrOfEnemies; i++)
+        //{
+        //    SpawnEnemy();
+        //}
+        
+        while (nrOfEnemies < 2)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnEnemy()
+    {
+        GameObject newEnemy = Instantiate(EnemyPrefab);
+
+        newEnemy.transform.position = new Vector2(Random.Range(-6f, 11f), Random.Range(-6f, 11f));
+    }
+}
