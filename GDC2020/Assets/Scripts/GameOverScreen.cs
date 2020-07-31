d7abf8580960ec74753d1be095e6be9f86f64ffb
WildclawsDK <alex-thomsen@hotmail.com>
2020-07-31 15:56:14 +0200

    Changed point count

--- GDC2020/Assets/Scripts/GameOverScreen.cs
+++ GDC2020/Assets/Scripts/GameOverScreen.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameOverScreen : MonoBehaviour
 {
+    public Text scoreText;
+    
+
     // Start is called before the first frame update
     void Start()
     {
+        PointCount Score = FindObjectOfType<PointCount>();
         Cursor.visible = true;
+        scoreText.text = "You scored: " + Score.points;
+
+        Destroy(Score.gameObject);
+
     }
 
     // Update is called once per frame
